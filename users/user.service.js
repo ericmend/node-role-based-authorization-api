46ab4242b1b6d2c8c3adaf0186b9771d03880f17
Eric Azevedo Mendonça <ericmend@gmail.com>
2021-03-03 11:07:35 -0300

    adicionando tempo de expiraçã do token

--- users/user.service.js
+++ users/user.service.js
@@ -17,7 +17,7 @@ module.exports = {
 async function authenticate({ username, password }) {
     const user = users.find(u => u.username === username && u.password === password);
     if (user) {
-        const token = jwt.sign({ sub: user.id, role: user.role }, config.secret);
+        const token = jwt.sign({ sub: user.id, role: user.role }, config.secret, config.expiresIn ? { expiresIn: config.expiresIn } : null );
         const { password, ...userWithoutPassword } = user;
         return {
             ...userWithoutPassword,
